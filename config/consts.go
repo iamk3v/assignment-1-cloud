14c57cab87fba3f367ca637adabf2ba7519d6511
Marius Aga <mnaga@stud.ntnu.no>
2025-02-27 10:40:38 +0100

    Fixed routes

--- config/consts.go
+++ config/consts.go
@@ -1,6 +1,6 @@
 package config
 
-const START_URL = " /countryinfo/" + VERSION
+const START_URL = "/countryinfo/" + VERSION
 const COUNTRIESNOW_ROOT = "http://129.241.150.113:3500/api/v0.1/"
 const RESTCOUNTRIES_ROOT = "http://129.241.150.113:8080/v3.1/"
 const VERSION = "v1"
--- main.go
+++ main.go
@@ -18,20 +18,20 @@ func main() {
 	router := http.NewServeMux()
 
 	// Routes
-	router.HandleFunc(http.MethodGet+config.START_URL+"/info/", handlers.InfoHandler) // Root path
-	router.HandleFunc(http.MethodGet+config.START_URL+"/info/{two_letter_country_code}", handlers.InfoHandler)
-	router.HandleFunc(http.MethodGet+config.START_URL+"/population/", handlers.PopulationHandler) // Root path
-	router.HandleFunc(http.MethodGet+config.START_URL+"/population/{two_letter_country_code}", handlers.PopulationHandler)
-	router.HandleFunc(http.MethodGet+config.START_URL+"/status/", handlers.StatusHandler) // Root path
-	// Handle all 404 if no match found
-	router.HandleFunc(http.MethodGet+config.START_URL+"/", handlers.NotFoundHandler)
+	router.HandleFunc(config.START_URL+"/info/", handlers.InfoHandler) // Root path
+	router.HandleFunc(config.START_URL+"/info/{two_letter_country_code}", handlers.InfoHandler)
+	router.HandleFunc(config.START_URL+"/population/", handlers.PopulationHandler) // Root path
+	router.HandleFunc(config.START_URL+"/population/{two_letter_country_code}", handlers.PopulationHandler)
+	router.HandleFunc(config.START_URL+"/status/", handlers.StatusHandler) // Root path
+	//Handle all 404 if no match found
+	router.HandleFunc("/", handlers.NotFoundHandler)
 
 	// Define a port and check whether the OS have a designated port or not
 	PORT := "8080"
 	if os.Getenv("PORT") != "" {
 		PORT = os.Getenv("PORT")
 	}
-	
+
 	// Listen on the designated port for traffic
 	log.Println("Starting server on port " + PORT)
 	err := http.ListenAndServe(":"+PORT, router)
